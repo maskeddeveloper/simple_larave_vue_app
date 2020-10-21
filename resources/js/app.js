2b63cf0d80cd84523b8a85e9d92386a6a226a734
Mehdi Hamime <mhamime@cloud.deloitte.fr>
2020-10-21 16:08:29 +0100

    Last

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,10 +19,10 @@ import App from "./App.vue";
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('App', require('./App.vue').default);
 Vue.component('modal', {
     template: '#modal-template'
-  })
+})
 
 
 /**
